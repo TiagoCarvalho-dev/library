330f03f31151dc81a6b3f9d02eaeed7ebe831b23
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-07-14 23:03:16 -0300

    Started adding the functions to add books and book cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myLibrary = [];
 
-function Books(title, author, genre, observation) {
+function Book(title, author, genre, observation) {
   this.title = title,
   this.author = author,
   this.genre = genre,
@@ -9,10 +9,36 @@ function Books(title, author, genre, observation) {
   this.favorite = false
 }
 
-Books.prototype.toggleReadStatus = function() {
+Book.prototype.toggleReadStatus = function() {
   this.read ? this.read = false : this.read = true;
 }
 
-Books.prototype.toggleFavoriteStatus = function {
+Book.prototype.toggleFavoriteStatus = function (){
   this.favorite ? this.favorite = false : this.favorite = true;
+}
+
+const form = document.querySelector('.add-book-form');
+
+document.querySelector('.add-book-button').addEventListener('click', addBook);
+
+function addBook() {
+  let book = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['genre'].value, form.elements['observation'].value);
+  myLibrary.push(book);
+  console.log(myLibrary);
+  makeBookCard();
+  event.preventDefault();
+}
+
+function makeBookCard() {
+  myLibrary.forEach(book => {
+    document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card');
+    document.querySelector('.book-card').appendChild(document.createElement('h4')).textContent = book.title;
+    document.querySelector('.book-card').appendChild(document.createElement('p')).textContent = book.author;
+    document.querySelector('.book-card').appendChild(document.createElement('p')).textContent = book.observation;
+    document.querySelector('.book-card').appendChild(document.createElement('p')).textContent = book.genre;
+    document.querySelector('.book-card').appendChild(document.createElement('div')).classList.add('card-buttons-container');
+    document.querySelector('.card-buttons-container').appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container').appendChild(document.createElement('button')).setAttribute('type', 'button');
+    event.preventDefault();
+  });
 }
