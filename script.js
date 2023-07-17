f0b54ad24b926cde4cc15983521620187202a458
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-07-17 20:30:11 -0300

    Added function to display all cards on page load
    Also added a function to toggle the display of items when the new book button is pressed.

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let myLibrary = [];
+// Hardcoded books used for developing
+
+let book1 = new Book('Book0', 'Author0', 'genre0', 'this book is nice', true, false);
+let book2 = new Book('Book1', 'Author1', 'genre1', 'this book sucks', false, false);
+let book3 = new Book('Book2', 'Author2', 'genre2', 'this book is alright', false, true);
+
+// End
+
+let myLibrary = [book1, book2, book3];
 
 function Book(title, author, genre, observation) {
   this.title = title,
@@ -17,9 +25,13 @@ Book.prototype.toggleFavoriteStatus = function (){
   this.favorite ? this.favorite = false : this.favorite = true;
 }
 
+showAllCards();
+
 const form = document.querySelector('.add-book-form');
 
 document.querySelector('.add-book-button').addEventListener('click', addBook);
+
+document.querySelector('.new-button').addEventListener('click', () => form.classList.toggle('hidden'));
 
 function addBook() {
   let book = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['genre'].value, form.elements['observation'].value);
@@ -30,12 +42,33 @@ function addBook() {
 }
 
 function makeBookCard() {
-    document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + (myLibrary.length - 1));
-    document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('h4')).textContent = myLibrary[(myLibrary.length - 1)].title;
-    document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].author;
-    document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].observation;
-    document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].genre;
-    document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + (myLibrary.length - 1));
-    document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).appendChild(document.createElement('button')).setAttribute('type', 'button');
-    document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).appendChild(document.createElement('button')).setAttribute('type', 'button');
+  document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + (myLibrary.length - 1));
+  document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('h4')).textContent = myLibrary[(myLibrary.length - 1)].title;
+  document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].author;
+  document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].observation;
+  document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('p')).textContent = myLibrary[(myLibrary.length - 1)].genre;
+  document.querySelector('.book-card-' + (myLibrary.length - 1)).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + (myLibrary.length - 1));
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).appendChild(document.createElement('button')).setAttribute('type', 'button');
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).firstChild.textContent = 'READ';
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).appendChild(document.createElement('button')).setAttribute('type', 'button');
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).firstChild.nextSibling.textContent = 'FAVORITE';
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).appendChild(document.createElement('button')).setAttribute('type', 'button');
+  document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).lastChild.textContent = 'ERASE';
+}
+
+function showAllCards() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].observation;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].genre;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + i);
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).firstChild.textContent = 'READ';
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).firstChild.nextSibling.textContent = 'FAVORITE';
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).lastChild.textContent = 'ERASE';
+  }
 }
