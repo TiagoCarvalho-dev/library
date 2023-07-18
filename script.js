bb92b1a3af3e4298aa7a362411537ecf017200df
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-07-17 22:27:15 -0300

    Added a function to filter books by genre

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Hardcoded books used for developing
 
-let book0 = new Book('Book0', 'Author0', 'Fantasy', 'this book is nice', true, false);
-let book1 = new Book('Book1', 'Author1', 'Fiction', 'this book sucks', false, false);
-let book2 = new Book('Book2', 'Author2', 'Biography', 'this book is alright', false, true);
+let book0 = new Book('Book0', 'Author0', 'fantasy', 'this book is nice', true, false);
+let book1 = new Book('Book1', 'Author1', 'fiction', 'this book sucks', false, false);
+let book2 = new Book('Book2', 'Author2', 'biography', 'this book is alright', false, true);
 
 // End
 
@@ -36,6 +36,8 @@ document.querySelector('.apply-button').addEventListener('click', () => {
     showAllBookCards();
   } else if (document.querySelector('#filter').value === 'z-a') {
     showAllBookCardsReversed();
+  } else if (document.querySelector('#filter').value === 'genre') {
+    showGenreBookCards();
   } else if (document.querySelector('#filter').value === 'read') {
     showReadBookCards();
   } else if (document.querySelector('#filter').value === 'favorite') {
@@ -157,6 +159,26 @@ function showSearchedBookCards() {
   clearAllCards();
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].title === document.querySelector('#search').value || myLibrary[i].author == document.querySelector('#search').value) {
+      document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].observation;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].genre;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + i);
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).firstChild.textContent = 'READ';
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).firstChild.nextSibling.textContent = 'FAVORITE';
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).lastChild.textContent = 'ERASE';
+    }
+  }
+}
+
+function showGenreBookCards() {
+  clearAllCards();
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].genre === document.querySelector('#filter-genre').value) {
       document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
       document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
       document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
