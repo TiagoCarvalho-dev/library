9f9390e50db16b33a5c58e760d7b401522db9d6b
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-07-17 21:11:04 -0300

    Added function to show all cards in reverse order
    Also added a reset function to the add book button, that returns all values from the form to default.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Hardcoded books used for developing
 
-let book1 = new Book('Book0', 'Author0', 'genre0', 'this book is nice', true, false);
-let book2 = new Book('Book1', 'Author1', 'genre1', 'this book sucks', false, false);
-let book3 = new Book('Book2', 'Author2', 'genre2', 'this book is alright', false, true);
+let book1 = new Book('Book0', 'Author0', 'Fantasy', 'this book is nice', true, false);
+let book2 = new Book('Book1', 'Author1', 'Fiction', 'this book sucks', false, false);
+let book3 = new Book('Book2', 'Author2', 'Biography', 'this book is alright', false, true);
 
 // End
 
@@ -38,6 +38,10 @@ function addBook() {
   myLibrary.push(book);
   console.log(myLibrary);
   makeBookCard();
+  form.elements['title'].value = '';
+  form.elements['author'].value = '';
+  form.elements['genre'].value = 'biography';
+  form.elements['observation'].value = '';
   event.preventDefault();
 }
 
@@ -58,6 +62,23 @@ function makeBookCard() {
 
 function showAllCards() {
   for (let i = 0; i < myLibrary.length; i++) {
+    document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].observation;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].genre;
+    document.querySelector('.book-card-' + i).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + i);
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).firstChild.textContent = 'READ';
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).firstChild.nextSibling.textContent = 'FAVORITE';
+    document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+    document.querySelector('.card-buttons-container-' + i).lastChild.textContent = 'ERASE';
+  }
+}
+
+function showAllCardsReversed() {
+  for (let i = myLibrary.length - 1; i >= 0; i--) {
     document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
     document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
     document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
