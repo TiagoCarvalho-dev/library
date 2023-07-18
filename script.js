080a26b0b5f88129fb59a9171c58668865927965
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-07-17 22:00:14 -0300

    Added function to show all the read books

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Hardcoded books used for developing
 
-let book1 = new Book('Book0', 'Author0', 'Fantasy', 'this book is nice', true, false);
-let book2 = new Book('Book1', 'Author1', 'Fiction', 'this book sucks', false, false);
-let book3 = new Book('Book2', 'Author2', 'Biography', 'this book is alright', false, true);
+let book0 = new Book('Book0', 'Author0', 'Fantasy', 'this book is nice', true, false);
+let book1 = new Book('Book1', 'Author1', 'Fiction', 'this book sucks', false, false);
+let book2 = new Book('Book2', 'Author2', 'Biography', 'this book is alright', false, true);
 
 // End
 
-let myLibrary = [book1, book2, book3];
+let myLibrary = [book0, book1, book2];
 
 function Book(title, author, genre, observation) {
   this.title = title,
@@ -25,13 +25,27 @@ Book.prototype.toggleFavoriteStatus = function (){
   this.favorite ? this.favorite = false : this.favorite = true;
 }
 
-showAllCards();
+showAllBookCards();
 
 const form = document.querySelector('.add-book-form');
 
+document.querySelector('.new-book-button').addEventListener('click', () => form.classList.toggle('hidden'));
 document.querySelector('.add-book-button').addEventListener('click', addBook);
+document.querySelector('.apply-button').addEventListener('click', () => {
+  if (document.querySelector('#filter').value === 'a-z') {
+    showAllBookCards();
+  } else if (document.querySelector('#filter').value === 'z-a') {
+    showAllBookCardsReversed();
+  } else if (document.querySelector('#filter').value === 'read') {
+    showAllReadBookCards();
+  }
+});
 
-document.querySelector('.new-button').addEventListener('click', () => form.classList.toggle('hidden'));
+function clearAllCards() {
+  while (document.querySelector('.book-cards').firstChild) {
+    document.querySelector('.book-cards').removeChild(document.querySelector('.book-cards').lastChild);
+  }
+}
 
 function addBook() {
   let book = new Book(form.elements['title'].value, form.elements['author'].value, form.elements['genre'].value, form.elements['observation'].value);
@@ -60,7 +74,8 @@ function makeBookCard() {
   document.querySelector('.card-buttons-container-' + (myLibrary.length - 1)).lastChild.textContent = 'ERASE';
 }
 
-function showAllCards() {
+function showAllBookCards() {
+  clearAllCards();
   for (let i = 0; i < myLibrary.length; i++) {
     document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
     document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
@@ -77,7 +92,8 @@ function showAllCards() {
   }
 }
 
-function showAllCardsReversed() {
+function showAllBookCardsReversed() {
+  clearAllCards();
   for (let i = myLibrary.length - 1; i >= 0; i--) {
     document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
     document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
@@ -91,5 +107,25 @@ function showAllCardsReversed() {
     document.querySelector('.card-buttons-container-' + i).firstChild.nextSibling.textContent = 'FAVORITE';
     document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
     document.querySelector('.card-buttons-container-' + i).lastChild.textContent = 'ERASE';
+  }
+}
+
+function showAllReadBookCards() {
+  clearAllCards();
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].read) {
+      document.querySelector('.book-cards').appendChild(document.createElement('div')).classList.add('book-card-' + i);
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('h4')).textContent = myLibrary[i].title;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].author;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].observation;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('p')).textContent = myLibrary[i].genre;
+      document.querySelector('.book-card-' + i).appendChild(document.createElement('div')).classList.add('card-buttons-container-' + i);
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).firstChild.textContent = 'READ';
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).firstChild.nextSibling.textContent = 'FAVORITE';
+      document.querySelector('.card-buttons-container-' + i).appendChild(document.createElement('button')).setAttribute('type', 'button');
+      document.querySelector('.card-buttons-container-' + i).lastChild.textContent = 'ERASE';
+    }
   }
 }
